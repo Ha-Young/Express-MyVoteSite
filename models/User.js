c30daed322e20db842df14f5dfc3dacbd0bd6854
Juyeon <jy7123943@gmail.com>
2019-09-23 17:43:23 +0900

    feat: User Schema structure has changed

--- models/User.js
+++ models/User.js
@@ -1,36 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/*
-  password: 최소 하나의 문자 + 하나의 숫자 + 하나의 특수문자 포함 6자리 이상
-*/
 const userSchema = new Schema({
   email: {
     type: String,
+    trim: true,
     required: [true, 'Email required'],
-    validate: {
-      validator: function (v) {
-        const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-        return emailRegex.test(v);
-      },
-      message: props => `${props.value} is not a valid email.`
-    }
+    match: [/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i, 'Not a valid email']
   },
   password: {
     type: String,
+    trim: true,
     required: [true, 'Password required'],
-    validate: {
-      validator: function (v) {
-        const emailRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/;
-        return emailRegex.test(v);
-      },
-      message: props => `${props.value} is not a valid password.`
-    }
+    match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/, 'Not a valid password']
   },
   name: {
     type: String,
+    trim: true,
     required: [true, 'Name required'],
-    trim: true
+    match: [/^[가-힣]{2,4}|[a-zA-Z]{2,20}$/, 'Not a valid name']
+  },
+  salt: {
+    type: String,
+    require: true
   }
 });
 
