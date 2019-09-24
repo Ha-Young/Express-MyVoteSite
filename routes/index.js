fc803c4e1c89f4c2855eac5609dc207c973aec1e
hakyung <coding891@gmail.com>
2019-09-24 21:23:13 +0900

    Add register

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', message: req.flash('message') });
 });
 
-router.get('/login', function (req, res, next) {
-  res.render('login', { title: 'Login'})
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
-router.get('/register', function (req, res, next) {
-  res.render('register', { title: 'Register'})
-})
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register', message: req.flash('message') });
+});
+
+router.post('/register', function(req, res, next) {
+  const { name, email, password } = req.body;
+  User.findOne({ email: email }).then(user => {
+    if (!user) {
+      const newUser = new User({
+        name,
+        email,
+        password,
+        created_at: new Date()
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              req.flash('message', '등록되었습니다.');
+              res.redirect('/');
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    } else {
+      req.flash('message', '이미 있는 아이디입니다');
+      res.redirect('/register');
+    }
+  });
+});
 
 module.exports = router;
