1bbaf9f72e071957b4b31e1cf005eb1a766d783f
hakyung <coding891@gmail.com>
2019-09-25 19:49:48 +0900

    Add votings/new

---
+++ models/Voting.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const votingSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    user: { type: String, required: true },
+    expiration: { type: String, required: true },
+    items: [
+      {
+        option: { type: String, required: true },
+        voters: [mongoose.Schema.Types.ObjectId],
+      }
+    ]
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+);
+
+module.exports = mongoose.model('Voting', votingSchema);
--- routes/votings.js
+++ routes/votings.js
@@ -7,7 +7,36 @@ router.get('/new', function(req, res, next) {
   res.render('new');
 });
 
+router.get('/success', function(req, res, next) {
+  res.render('success', { message: '투표생성성공' });
+});
+
+router.get('/error', function(req, res, next) {
+  res.render('error', { message: '투표생성실패' });
+});
+
 router.post('/new', function(req, res, next) {
+  const { title, expiration, option, description } = req.body;
+  const item = [];
+  for (let i = 0; i < option.length; i++) {
+    let opt = { option: option[i], voters: [] };
+    item.push(opt);
+  }
+  const newVoting = new Votings({
+    title,
+    description,
+    user: req.user._id,
+    expiration,
+    items: item
+  });
+  newVoting
+    .save()
+    .then(user => {
+      res.redirect('/votings/success');
+    })
+    .catch(err => {
+      res.redirect('/votings/error');
+    });
 });
 
 module.exports = router;
