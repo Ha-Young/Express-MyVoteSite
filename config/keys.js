f7c30771cb2b795fa89a976cc9d8714bf2d2922e
Imho Jang <imhome606@gmail.com>
2019-09-27 23:14:55 +0900

    fixed mongoURL

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@
-  cluster0-rlera.mongodb.net/voting_platform?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-rlera.mongodb.net/voting_platform?retryWrites=true&w=majority`,
 };
