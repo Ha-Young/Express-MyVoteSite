adfbe6cde2d2b43f3119c999375a27126f03e18a
dwook <igerapex@gmail.com>
2019-09-28 00:25:51 +0900

    Remove favicon setting

--- voting/app.js
+++ voting/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -31,12 +30,9 @@ mongoose.connect(
 const app = express();
 passportConfig(passport);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.json());
