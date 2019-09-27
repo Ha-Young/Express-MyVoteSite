5ab501574b570b5ed033e6daaddca94e543b31de
Simon <sijeecodes@gmail.com>
2019-09-27 19:18:14 +0900

    update error procedures in /votings/new, delete node_modules

--- routes/votings.js
+++ routes/votings.js
@@ -27,11 +27,34 @@ router.get('/', authCheck, async (req, res, next) => {
 });
 
 router.get('/new', authCheck, (req, res, next) => {
+// 투표 제목, 투표 선택 사항, 만료 날짜 및 시간을 입력할 수 있어야 합니다.
+// 투표 목록으로 돌아갈 수 있는 버튼이 있어야 합니다.
+// 선택 사항은 반드시 2개 이상이어야 생성이 가능합니다.
+// 투표를 생성하게 되면 메인 페이지의 전체 투표 목록에 반영되고 누구나 투표가 가능합니다.
+// 투표 생성 직후 사용자는 메인 페이지로 이동합니다.
   res.render('votings/new', { title: 'A New Voting' });
 });
 
 router.post('/new', authCheck, async (req, res, next) => {
-  if (req.body.options.length < 2) {
+  if (req.body.title === undefined) {
+    res.render('votings/new', {
+      title: 'A New Voting',
+      message: 'Please input title'
+    });
+  }
+  if (req.body.description === undefined) {
+    res.render('votings/new', {
+      title: 'A New Voting',
+      message: 'Please input description'
+    });
+  }
+  if (req.body.duration === undefined) {
+    res.render('votings/new', {
+      title: 'A New Voting',
+      message: 'Please input voting hours'
+    });
+  }
+  if (req.body.options === undefined || req.body.options.length < 2) {
     res.render('votings/new', {
       title: 'A New Voting',
       message: 'You need at least two options'
@@ -62,11 +85,14 @@ router.post('/new', authCheck, async (req, res, next) => {
 });
 
 router.get('/success', authCheck, (req, res, next) => {
-  res.render('votings/success', { title: 'Success' });
+  res.render('votings/success', { title: 'success' });
 });
 
 router.get('/error', authCheck, (req, res, next) => {
-  res.render('votings/error', { title: 'Failed to create a new voting'})
+// 투표 생성을 하지 못했다는 실패 메시지가 표기 되어야 합니다.
+// 메인 페이지로 돌아갈 수 있는 버튼 혹은 링크가 있어야 합니다.
+// 상세 에러 내용(Stack 정보 등)을 사용자에게 보여주어선 안됩니다.
+  res.render('votings/error', { title: 'failed to create a new voting'})
 });
 
 router.get('/:id', authCheck, async (req, res, next) => {
