01495dbb92e2da58710b90c1aa1065dd21ebf0c7
yami03 <seula00027@gmail.com>
2019-09-28 23:36:35 +0900

    Modified deserializeUser function

--- config/passport.js
+++ config/passport.js
@@ -8,12 +8,16 @@ passport.serializeUser((user, done) => {
   done(null, user.email);
 });
 
-passport.deserializeUser((email, done) => {
-  User.findOne({ email })
-      .then(user => {
-        done(null, user);
-      })
-      .catch(err => done(err));
+passport.deserializeUser(async (email, done) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return done(new Error('가입되지 않은 회원입니다.'));
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 });
 
 passport.use(new LocalStrategy({
