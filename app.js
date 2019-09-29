31e7d2781b46c1a1b096b00634e873f1c8109ee8
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-09-29 18:43:11 +0900

    Modified Styles and order  app.js

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const lessMiddleware = require('less-middleware');
 const passport = require('passport');
 const session = require('express-session');
 const passportConfig = require('./passport');
-passportConfig(passport);
 const flash = require('connect-flash');
 const path = require('path');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const votingRouter = require('./routes/votings');
-const app = express();
 
-// dotenv requring in only development env
+const app = express();
+passportConfig(passport);
+
+// Dotenv requring in only development env
+
 if (process.env.NODE_ENV === 'development') {
   const dotenv = require('dotenv');
   dotenv.config({
@@ -24,20 +26,23 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-// mongoose setup
+// Mongoose setup
+
 mongoose.connect(process.env.MONGOOSE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// mongoose connecton
+// Mongoose connecton
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('We are connected!');
 });
 
-// basic modules
+// Basic modules
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
@@ -59,24 +64,29 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// routers
+// Routers
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/votings', votingRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
+
 app.use(function(err, req, res, next) {
 
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
