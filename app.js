9a5b75e5665ce80e384c3df80e923d812aa9143c
Jeong <inyeop0212@gmail.com>
2020-03-16 15:58:56 +0900

    Set babel, lint, scripts

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+import createError from 'http-errors';
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
