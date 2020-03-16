8b3246a968d680ee1d28330d03d4929e2668a019
Joy Park <soodang@Joyui-MacBookPro.local>
2020-03-16 17:34:21 +0900

    basic ui frame:sign up

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'voting-app' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "voting-app" });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { style: 'login' ,title: 'voting-app' });
+router.get("/login", function(req, res, next) {
+  res.render("login", { title: "voting-app", style: "login" });
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'voting-app' });
+router.get("/signup", function(req, res, next) {
+  res.render("signup", { title: "voting-app", style: "login" });
 });
-
-
 
 module.exports = router;
