72b48d53bae38dfaec8cf42b1b258c35aaa8b74d
kimraeyoung <760kry@gmail.com>
2020-03-17 00:32:34 +0900

    미들웨어 추가

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware');
 
-
-router.get('/', function (req, res, next) {
+router.get('/', isLoggedIn, (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
