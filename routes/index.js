97665b9802ff71ef8299fbbe4aeb4cd3b217333b
Joy Park <soodang@Joyui-MacBookPro.local>
2020-03-16 16:33:58 +0900

    initialize folder

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'voting-app' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'voting-app' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'voting-app' });
+});
+
+
+
+module.exports = router;
