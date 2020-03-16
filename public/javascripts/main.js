0c78fc72b55566f24ebfb6ae7c67f09278bd0e58
Jeong <inyeop0212@gmail.com>
2020-03-16 23:04:07 +0900

    Render newVoting

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,45 @@
-console.log('Hello?');
+const optionsArea = document.querySelector('.options-area');
+const addOptionButton = document.querySelector('.add-options');
+const removeOptionButton = document.querySelector('.remove-options');
+const confirm = document.querySelector('.confirm');
+
+if (optionsArea) {
+  let optionCount = 1;
+
+  addOptionButton.addEventListener('click', e => {
+    e.preventDefault();
+    optionCount++;
+    addOptionItem(optionCount, optionsArea);
+  });
+
+  removeOptionButton.addEventListener('click', e => {
+    e.preventDefault();
+    if (optionCount > 1) {
+      optionCount--;
+      removeOptionItem(optionsArea);
+    }
+  });
+}
+
+if (confirm) {
+  confirm.addEventListener('click', e => {
+    if (!window.confirm('제출하시겠습니까?')) {
+      e.preventDefault();
+    }
+  })
+}
+
+function addOptionItem (count, target) {
+  const optionItem = `<input type="text" name="option${count}" />`;
+  const p = document.createElement('p');
+
+  p.innerHTML = optionItem;
+  target.append(p);
+}
+
+function removeOptionItem (target) {
+  const options = target.querySelectorAll('p');
+  const lastIndex = options.length - 1;
+
+  target.removeChild(options[lastIndex]);
+}
--- routers/votingRouter.js
+++ routers/votingRouter.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/new', (req, res) => {
+  res.render('newVoting');
+});
+
 router.get('/:id', (req, res) => {
   res.render('votingDetail');
 });
