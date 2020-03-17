4a6b8a886a38a4ffecee48d1a45f110b1d612aaf
kimraeyoung <760kry@gmail.com>
2020-03-18 00:47:30 +0900

    POST/votings/new 과거 날짜 선택시 경고메세지 구현

--- routes/votings.js
+++ routes/votings.js
@@ -1,36 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const { isLoggedIn, votingCreate } = require('../middleware');
+const { isLoggedIn } = require('../middleware');
 const Votings = require('../models/Votings');
 
 router.get('/new', isLoggedIn, (req, res, next) => {
     res.render('newVoting', {
-        title: '투표 생성하기'
+        title: '투표 생성하기',
+        message: ''
     });
 });
 
 router.post('/new', async (req, res, next) => {
+    const todayNow = new Date();
     const { title, description, expiration_date, expiration_time, selectOption } = req.body;
-    const result_expiration_date = expiration_date + '.' +expiration_time;
+    const result_expiration_date = new Date(expiration_date + '.' + expiration_time);
     const select_option = selectOption.split(',');
 
     try {
+        if (todayNow > result_expiration_date) {
+            req.flash('dateError', '날짜를 확인하세요.')
+            return res.render('newVoting', {
+                title: '투표 생성하기',
+                message: req.flash('dateError')
+            });
+        }
         await Votings.create({
             id: Date.now(),
-            title: title,
+            title,
             writer: req.user.name,
             writerId: req.user._id,
-            description: description,
-            expiration_date: new Date(result_expiration_date),
-            select_option: select_option,
+            description,
+            expiration_date: result_expiration_date,
+            select_option,
             progress: true
         });
         return res.redirect('/');
 
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
-
 });
 
 router.get('/:id', isLoggedIn, (req, res, next) => {
