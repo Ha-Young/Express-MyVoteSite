91fbe266e6c1557bbe93c0bfd9b769f8faf39c6f
thms200 <thms0504@gmail.com>
2020-03-19 19:28:54 +0900

    modify authorization: due to user controller

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,6 +1,6 @@
 function ensureAuthenticated (req, res, next) {
   if (req.isAuthenticated()) return next();
-  res.status(302).redirect('/login');
+  return res.status(302).redirect('/users/login');
 }
 
 exports.ensureAuthenticated = ensureAuthenticated;
