a1ddc85092395ae96d45a39ccade1d52a100115c
nathaneast <ahrl_ahrl@naver.com>
2020-03-19 10:30:27 +0900

    투표 페이지 생성자 커스텀기능 작업 해야함

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,20 @@ const Voting = require('../models/Voting');
 
 router.get('/', async (req, res, next) => {
   const allVotings = await Voting.find();
+
+  
   
   res.render('index', {
+    //FIXME: 기간 만료, 진행중 계산해서 ejs에 넘겨주기 
     allVotings,
-    currentDate: new Date().getTime()
+    currentDate: new Date().getTime(),
+    user: req.user
   });
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/votings.js
+++ routes/votings.js
@@ -4,12 +4,7 @@ const router = express.Router();
 const Voting = require('../models/Voting');
 
 router.get('/new', (req, res, next) => {
-  //FIXME: 더 효율적인 방법으로 수정하기, 모달
-  if (req.user) {
     res.render('newVoting');
-  } else {
-    res.redirect('/');
-  }
 });
 
 router.post('/new', async (req, res, next) => {
@@ -42,12 +37,17 @@ router.post('/new', async (req, res, next) => {
 
 router.get('/:voting_id', async (req, res) => {
   const { voting_id: id } = req.params;
-
-  //해당 투표값
   const voting = await Voting.findOne({ _id: id });
   const votingEndDate = new Date(voting.endDate).getTime();
   const currentDate = new Date().getTime();
-  const isAuthor = req.user.email === voting.author;
+  let isAuthor;
+
+  if(req.user) {
+    isAuthor = req.user.email === voting.author;
+  } else {
+    isAuthor = null;
+  }
+  
 
   if (votingEndDate >= currentDate) {
     // 만료 X => 해당 투표 페이지 보여주기
@@ -67,18 +67,3 @@ router.get('/:voting_id', async (req, res) => {
 });
 
 module.exports = router;
-
-/*
-  {
-  solvedUser: [],
-  _id: 5e7226e8cccc5f0c40549b8a,
-  title: 'q',
-  items: [
-    { _id: 5e7226e8cccc5f0c40549b8b, name: 'q1', count: 0 },
-    { _id: 5e7226e8cccc5f0c40549b8c, name: 'q2', count: 0 }
-  ],
-  endDate: '2020-03-26T01:01',
-  author: 'bb@bb.com',
-  __v: 0
-}
-*/
