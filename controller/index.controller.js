d9b1e9f319adc0c092a39abb54f93859dee94953
thms200 <thms0504@gmail.com>
2020-03-19 21:47:45 +0900

    refactoring index controller

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,27 +1,16 @@
 const createError = require('http-errors');
 const Voting = require('../models/Voting');
+const util = require('../lib/util');
 
 exports.getAll = async (req, res, next) => {
   try {
     const votes = await Voting.find().populate('made');
     if (!votes) return next(createError(404, 'We can not find the votes for unknown reasons. Try again.'));
-    const ongoingVotes = [];
-    const completedVotes = [];
-    votes.forEach((vote) => {
-      const expirationDate = vote.expiration_date.toString();
-      const voteInfo = {
-        id: vote._id,
-        title: vote.title,
-        made: vote.made.email.split('@')[0],
-        expiration: expirationDate.split('GMT')[0].slice(0, -4),
-      };
-      const isOngoing = new Date() < new Date(expirationDate);
-      isOngoing ? ongoingVotes.push(voteInfo) : completedVotes.push(voteInfo);
-    });
-    
-    if (!req.user) return res.render('index', { ongoingVotes, completedVotes });
+
+    const dividedVotes = util.divideVotes(votes);
+    if (!req.user) return res.render('index', { ongoingVotes: dividedVotes[0], completedVotes: dividedVotes[1] });
     const user = req.user.email.split('@')[0];
-    res.render('index', { user, ongoingVotes, completedVotes });
+    res.render('index', { user, ongoingVotes: dividedVotes[0], completedVotes: dividedVotes[1] });
   } catch (err) {
     next(err);
   }
@@ -31,24 +20,11 @@ exports.getMyVotings = async (req, res, next) => {
   try {
     const votes = await Voting.find().populate('made');
     if (!votes) return next(createError(404, 'We can not find the votes for unknown reasons. Try again.'));
-    const ongoingVotes = [];
-    const completedVotes = [];
-    votes.forEach((vote) => {
-      if (req.user._id.equals(vote.made._id)) {
-        const expirationDate = vote.expiration_date.toString();
-        const voteInfo = {
-          id: vote._id,
-          title: vote.title,
-          made: vote.made.email.split('@')[0],
-          expiration: expirationDate.split('GMT')[0].slice(0, -4),
-        };
-        const isOngoing = new Date() < new Date(expirationDate);
-        isOngoing ? ongoingVotes.push(voteInfo) : completedVotes.push(voteInfo);
-      }
-    });
-    if(!req.user) return res.render('index', { ongoingVotes, completedVotes });
+
+    const dividedVotes = util.divideVotes(votes, req.user._id);
+    if(!req.user) return res.render('index', { ongoingVotes: dividedVotes[0], completedVotes: dividedVotes[1] });
     const user = req.user.email.split('@')[0];
-    res.render('index', { user, ongoingVotes, completedVotes });
+    res.render('index', { user, ongoingVotes: dividedVotes[0], completedVotes: dividedVotes[1] });
   } catch (err) {
     next(err);
   }
