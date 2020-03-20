7c2ed3efeeaaab6c292104339d2e626705e09399
soldonii <dhs0113@gmail.com>
2020-03-20 21:48:43 +0900

    [refactoring]: fixed no-new-line and some commetns.

--- public/javascripts/newvotes.js
+++ public/javascripts/newvotes.js
@@ -55,19 +55,4 @@ const createVote = async (e) => {
       location.reload();
     }
   }
-
 };
-
-
-
-// form.addEventListener("submit", e => {
-//   e.preventDefault();
-//   let reqBody = {};
-//   Object.keys(form.elements).forEach(key => {
-//     let element = form.elements[key];
-//     if (element.type !== "submit") {
-//       reqBody[element.name] = element.value;
-//     }
-//   });
-//   submitForm(reqBody); //call to function for form submission
-// });
