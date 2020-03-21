96933f83d62c8e14d30b2389f04216e7512a3074
Jeong <inyeop0212@gmail.com>
2020-03-21 17:10:52 +0900

    Write Express test #1

--- test/backend.spec.js
+++ test/backend.spec.js
@@ -9,9 +9,34 @@ describe('GET /', () => {
       .expect('Content-Type', /html/)
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.include('Express');
+        if (err) {
+          return done(err);
+        }
+        expect(res.text).to.include('진행중인 투표' && '만료된 투표');
         done();
       });
+  });
+});
+
+describe('GET static assets', () => {
+  it('should be able to get static reset css file', () => {
+    request(app)
+      .get('/stylesheets/reset.css')
+      .expect('Content-Type', /css/)
+      .expect(200);
+  });
+
+  it('should be able to get static style css file', () => {
+    request(app)
+      .get('/stylesheets/styles.css')
+      .expect('Content-Type', /css/)
+      .expect(200);
+  });
+
+  it('should be able to get static js file', () => {
+    request(app)
+      .get('/javascripts/main.js')
+      .expect('Content-Type', /javascript/)
+      .expect(200);
   });
 });
