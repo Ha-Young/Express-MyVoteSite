e9c4ee59bfb71f514d22004205f42b681a0d7a22
nathaneast <ahrl_ahrl@naver.com>
2020-03-22 10:36:09 +0900

    home route 리팩토링

---
+++ routes/controllers/home.controllers.js
@@ -0,0 +1,41 @@
+const createError = require('http-errors');
+const Voting = require('../../models/Voting');
+
+exports.home = async (req, res, next) => {
+  try {
+    const allVotings = await Voting.find();
+
+    res.render('index', {
+      allVotings,
+      currentDate: new Date().getTime(),
+      user: req.user
+    });
+  } catch (err) {
+    next(createError({
+      message: '투표 목록을 불러올 수 없습니다'
+    }));
+  }
+}
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+};
+
+exports.myVotings = async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    const myVotingList = await Voting.find({ authorId: userId });
+    const currentDate = new Date().getTime();
+
+    res.render('myVotings', {
+      myVotingList, currentDate
+    });
+  } catch (error) {
+    next(createError({
+      message: '내가 등록한 투표를 찾을 수 없습니다.'
+    }));
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const Voting = require('../models/Voting');
+const homeControllers = require('./controllers/home.Controllers');
 
-router.get('/', async (req, res, next) => {
-  console.log('gha홈', res.locals)
-  const allVotings = await Voting.find();
-  res.render('index', {
-    allVotings,
-    currentDate: new Date().getTime(),
-    user: req.user
-  });
-});
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect('/');
-});
-
-router.get('/my-votings', async (req, res) => {
-  const userId = req.user._id;
-
-  try {
-    const myVotingList = await Voting.find({ authorId: userId });
-    const currentDate = new Date().getTime();
-
-    res.render('myVotings', {
-      myVotingList, currentDate
-    });
-  } catch (error) {
-    next(error)
-  }
-});
+router.get('/', homeControllers.home);
+router.get('/logout', homeControllers.logout);
+router.get('/my-votings', homeControllers.myVotings);
 
 module.exports = router;
