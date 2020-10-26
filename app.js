3c4c22d18f1dd4d4dc1cf510df2583ba4f620682
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-10-26 16:41:19 +0900

    feat: Controller getInfo

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 require('./db');
 const passport = require('passport');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var signupRouter = require('./routes/signup');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const signupRouter = require('./routes/signup');
+const loginRouter = require('./routes/login');
+const session = require('express-session');
+const app = express();
 
-var app = express();
+app.use(session({
+  secret: 'cats',
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 // view engine setup
@@ -21,12 +25,12 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/login', loginRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/controllers/signup.controller.js
+++ routes/controllers/signup.controller.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcrypt');
 
 exports.renderSignup = (req, res, next) => {
   res.render('signup');
 };
 
-exports.getInfo = (req, res, next) => {
-  const { body: { name, email, password } } = req;
-  console.log(email, name, password );
-  res.redirect('/login');
-};
+exports.getInfo = async(req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const existUser = await User.findOne({ email: email });
 
+    if (existUser) {
+      return res.render('join', { error: '존재하는 email 입니다' });
+    }
+    const hash = await bcrypt.hash(password, 12);
+    User({
+      name,
+      email,
+      password: hash,
+    }).save();
+
+    return res.redirect('/login');
+  } catch (err) {
+    next(err);
+  }
+};
