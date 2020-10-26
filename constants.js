e857835155245ec805209294b89c351cb3223a0e
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-10-26 21:05:21 +0900

    constants: Add signup error message

---
+++ constants.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    ERROR_MESSAGE_INVALID_ID: 'invalid article id',
+    ERROR_MESSAGE_INPUT_NAME: '한글만 입력가능합니다',
+    ERROR_MESSAGE_EXIST_EMAIL: '존재하는 email 입니다',
+    ERROR_MESSAGE_INPUT_EMAIL: '"@"를 포함해주세요',
+    ERROR_MESSAGE_INPUT_PASSWORD: '비밀번호가 일치하지 않습니다',
+});
