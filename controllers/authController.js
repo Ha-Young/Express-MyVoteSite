360254acdd7853d9b610eb991faa42bacc284876
KE LI <like19911209@hotmail.com>
2020-10-26 19:06:51 +0900

    signup post route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,7 @@
-exports.signup = () => {};
+exports.renderSignup = (req, res, next) => {
+  res.render('signup')
+};
+
+exports.registerUser = (req, res, next) => {
+  console.log('post')
+}
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/signup', authController.signup);
+
 
 router.get('/', function (req, res, next) {
   res.render('main', { data: 'welcome' });
 });
+
+router.get('/signup', authController.renderSignup);
+router.post('/signup', authController.registerUser);
 
 module.exports = router;
