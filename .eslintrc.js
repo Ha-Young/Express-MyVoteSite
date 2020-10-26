640eb7d5b21ee526ee7436a19d89b5348b4adb09
gyeongwoopark <geewoo94@gmail.com>
2020-10-26 14:16:37 +0900

    INIT: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'semi': [
+            'error',
+            'always'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'eol-last': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': 'none',
+    }
+};
