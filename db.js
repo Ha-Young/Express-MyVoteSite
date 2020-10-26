1b8d7cda76089db157b4eae2f8ed2d1694022075
JIHYUNHA <humm223@gmail.com>
2020-10-26 15:11:22 +0900

    feat: Build basic directory structure and Connect to database

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.ATLAS_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', error => console.error(`connection error: ${error}`));
+db.once('open', () => {
+  console.log('Connected to database');
+});
