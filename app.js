32740a722873d63dd803a37656789640a46bbf49
justindglee <nninnnin7@gmail.com>
2020-10-26 15:58:23 +0900

    initialize with express-generator, some modified

---
+++ app.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const db = require('./config/db');
+
+if (process.env.NODE_ENV === 'development') {
+  var dotenv = require('dotenv');
+  dotenv.config();
+}
+
+const indexRouter = require('./api/routes/index');
+const usersRouter = require('./api/routes/users');
+
+const app = express();
+
+db.connect();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ config/db.js
@@ -0,0 +1,17 @@
+const { Mongoose } = require('mongoose');
+
+const mongoose = require('mongoose');
+
+module.exports = {
+  connect: async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('몽구스가 성공적으로 서버를 몽고디비에 연결하였습니다');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
