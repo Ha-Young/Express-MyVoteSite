ce174ba82fc36c7c3e95a8572ea0eeb847dcbb9f
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-10-27 14:07:36 +0900

    Add routes

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const signupController = require('./controllers/signup.controller');
-/* GET home page. */
+
 router.get('/', signupController.renderSignup);
 router.post('/', signupController.saveUser);
+
 module.exports = router;
--- routes/votings.js
+++ routes/votings.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const votingController = require('./controllers/voting.controller');
+const { isLoggedIn } = require('../middleware/authorization');
 
-router.get('/new', votingController.renderNewVotingMakerPage);
+router.get('/new', isLoggedIn, votingController.renderNewVotingMakerPage);
 router.post('/:id');
-router.post('/options', votingController.addOption);
 module.exports = router;
