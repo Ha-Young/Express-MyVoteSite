857e7b70af56982078500fb1a092be8a46343bdb
justindglee <nninnnin7@gmail.com>
2020-10-27 21:43:32 +0900

    refactor: check if has user info with username before create user

--- services/user.js
+++ services/user.js
@@ -14,6 +14,17 @@ module.exports = class UserServices {
   }
 
   async createUser (userDTO) {
+    const isUser = await this.getUser(userDTO.username);
+
+    console.log(isUser);
+
+    if (isUser) {
+      return {
+        isFailed: true,
+        errorMsg: 'User is already registered!'
+      }
+    }
+
     const user = new User({
       _id: new mongoose.Types.ObjectId(),
       username: userDTO.username,
@@ -26,9 +37,6 @@ module.exports = class UserServices {
   }
 
   async authenticate (userDTO) {
-    // 유저로부터 username, pw를 받아온다
-
-    // 받아온 정보로 유저가 가입되어 있는지 확인한다
     const user = await this.getUser(userDTO.username);
 
     if (!user) {
@@ -40,23 +48,20 @@ module.exports = class UserServices {
 
     let result;
 
-    // 가입되어 있다면 비밀번호가 정확한지 확인해야 한다
     user.comparePassword(userDTO.password, (err, match) => {
       console.log('패스워드 일치? =>', match);
       if (!match) {
-        // 패스워드가 일치하지 않는다면..
         result = {
           isFailed: true,
           message: 'Wrong password'
         };
       } else {
         result = {
-          isFailed: false
+          isFailed: false,
+          userData: user
         };
       }
     });
-
-    // 성공했다면 세션을 만들고, 아이디를 쿠키에 담아 클라이언트 사이드로 보내준다
 
     return result;
   }
