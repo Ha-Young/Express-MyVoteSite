34884071cf7556787a485bcb172de1ef9488bb2e
gyeongwoopark <geewoo94@gmail.com>
2020-10-30 22:32:03 +0900

    de

--- controller/index.js
+++ controller/index.js
@@ -6,39 +6,6 @@ const logout = require('./logout');
 const myPage = require('./myPage');
 const votings = require('./votings');
 
-const render = (page, options) => {
-  return (req, res, next) => {
-    res.render(page, options);
-  };
-};
-
-const redirect = (page) => {
-  return (req, res, next) => {
-    res.redirect(page);
-  };
-};
-
-const json = (option, statusCode = 200) => {
-  return (req, res, next) => {
-    res.status(statusCode);
-    res.json(option);
-  };
-};
-
-const redirectBefore = () => {
-  return (req, res, next) => {
-    if (req.session.previousUrl) {
-      req.session.save(() => {
-        const tempUrl = req.session.previousUrl;
-        delete req.session.previousUrl;
-        res.redirect(tempUrl);
-      });
-    } else {
-      res.redirect('/');
-    }
-  };
-};
-
 module.exports = {
   error,
   home,
@@ -47,8 +14,4 @@ module.exports = {
   logout,
   myPage,
   votings,
-  render,
-  redirect,
-  json,
-  redirectBefore,
 };
