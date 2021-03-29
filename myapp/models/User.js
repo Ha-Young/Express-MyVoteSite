6dd306bd1f5975b3479d57e5115a7da084802402
kwon, minho <minhob38@gmail.com>
2021-03-29 20:15:18 +0900

    Implement Sign-in with crypto

--- myapp/models/User.js
+++ myapp/models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "이름을 입력하세요"] },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: { type: String, required: true },
 });
 
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const User = require("../models/User");
 
 router.get("/", function (req, res, next) {
   res.render("index");
@@ -10,10 +13,21 @@ router.get("/signup", function (req, res, next) {
   res.render("signup");
 });
 
-router.post("/signup", function (req, res, next) {
-  console.log(req.body);
+router.post("/signup", async function (req, res, next) {
+  const { name, email, password } = req.body;
+  const doc = await User.findOne({ email: email });
 
-  res.send("signup");
+  if (doc) {
+    res.send("존재하는 이메일입니다.");
+  } else {
+    const cryptoPassword = crypto
+      .createHash("sha512")
+      .update(password)
+      .digest("base64");
+
+    await User.create({ name, email, password: cryptoPassword });
+    res.send("signup");
+  }
 });
 
 module.exports = router;
