23cba758aeee44916bc8119a38b2368489ee789d
Parksoyoon <maudlinsy@gmail.com>
2021-03-29 15:08:51 +0900

    Use pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Voting Platform', message: "Let's Vote" });
+  res.render('main', { title: 'Voting Platform', message: 'header' });
 });
 
 module.exports = router;
