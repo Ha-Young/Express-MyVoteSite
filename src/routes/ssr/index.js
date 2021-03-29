43e6bd298eec339eeaaabb9c821b72368bbe6c42
ha-young <hayeong28@gmail.com>
2021-03-29 20:22:32 +0900

    add: home route

--- src/routes/ssr/index.js
+++ src/routes/ssr/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { HOME } = require("../../config/routes");
 
 const authRoute = require("./auth");
+const HomeController = require("./controller/home");
 
 module.exports = function () {
   const app = express.Router();
+
+  app.get(HOME, HomeController.home);
 
   authRoute(app);
 
