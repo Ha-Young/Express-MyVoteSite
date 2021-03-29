68d7d3efd48a6493e554ed3b5c7650d813886584
ha-young <hayeong28@naver.com>
2021-03-29 20:28:08 +0900

    fix: remove ssr route controller for readability

--- src/routes/ssr/index.js
+++ src/routes/ssr/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { HOME } = require("../../config/routes");
 
 const authRoute = require("./auth");
-const HomeController = require("./controller/home");
 
 module.exports = function () {
   const app = express.Router();
 
-  app.get(HOME, HomeController.home);
+  app.get(HOME, (req, res) => {
+    res.render("index");
+  });
 
   authRoute(app);
 
