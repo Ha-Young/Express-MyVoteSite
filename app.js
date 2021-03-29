dc09a14ad75b0bbd18a0ef91ee4ecf7a30e96447
KimJaedeok <jdcoder27@gmail.com>
2021-03-29 15:08:10 +0900

    initial init

---
+++ app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+switch (process.env.NODE_ENV) {
+  case "production":
+    app.use(logger("combined"));
+    break;
+  case "development":
+    app.use(logger("dev"));
+    break;
+  default:
+    app.use(logger("combined", { stream: accessLogStream }));
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
