19016537225d043632f1666c77a55adafd13ff78
ha-young <hayeong28@gmail.com>
2021-03-29 19:49:38 +0900

    add: route init

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const config = require("../config");
+const apiRoute = require("./api");
+const ssrRoute = require("./ssr");
+
+module.exports = app => {
+  app.use("/", ssrRoute());
+  app.use(config.api.prefix, apiRoute());
+};
