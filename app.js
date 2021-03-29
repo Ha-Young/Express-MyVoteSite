2c1e28267b9c42d7a01d8679d808371ddc5b2435
KYOUNGHOYU <qmfahs@naver.com>
2021-03-30 01:30:59 +0900

    Connect MongoDB Atlas

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const mongoose = require("mongoose");
+
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -34,7 +35,6 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
