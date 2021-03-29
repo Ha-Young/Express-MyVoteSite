e6a79285279173769a72e8fd7e7c6ab176361b1b
eluka22 <eluka22@gmail.com>
2021-03-29 17:53:03 +0900

    Add: bcrypt password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const createdVoteSchema = new mongoose.Schema({
   type: mongoose.Schema.Types.ObjectId
@@ -20,6 +21,21 @@ const userSchema = new mongoose.Schema({
     type: String
   },
   created_votes: [createdVoteSchema]
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
