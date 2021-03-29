b430bb537cc878078f6fdff2ca10ff8d69804c9f
yobe8 <sanyobe@gmail.com>
2021-03-29 16:42:25 +0900

    Add User schema

---
+++ model/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "이메일을 입력해주세요"],
+  },
+  password: {
+    type: String,
+    required: [true, "비밀번호를 입력해주세요"],
+  },
+  user_name: {
+    type: String,
+    required: [true, "사용자 이름(별명)을 입력해주세요"],
+  },
+  votings: [{
+    type: mongoose.ObjectId,
+    ref: "Vote",
+  }],
+}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+
+module.exports = mongoose.model("User", UserSchema);
