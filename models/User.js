b80bf0ebcf951f1b696583defa8fda6a960b26b1
ParkJeongbeen <pjb6510@gmail.com>
2021-03-30 00:11:16 +0900

    add encrypt password middleware in userSchema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, maxlength:20, require: true },
   email: { type: String, require: true, unique: true, index: true, },
   password: { type: String, },
-  isGoogleAccount: { type: Boolean, require: true },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
+      user.password = await bcrypt.hash(user.password, salt);
+
+      next();
+    } catch (err) {
+      return next(err);
+    }
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
