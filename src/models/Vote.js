90848c4a9854811634d623661c69c94c28820c5d
ha-young <hayeong28@naver.com>
2021-03-30 21:30:30 +0900

    add: Vote Scheme

---
+++ src/models/Vote.js
@@ -0,0 +1,23 @@
+const { Joi } = require("celebrate");
+const mongoose = require("mongoose");
+const joigoose = require("joigoose")(mongoose);
+
+Joi.objectId = require('joi-objectid')(Joi);
+
+const joiVoteOptionSchema = Joi.object.keys({
+  title: Joi.string().required(),
+  count: Joi.number(),
+});
+
+const joiVoteSchema = Joi.object({
+  title: Joi.string().required(),
+  creator: Joi.objectId().required(),
+  expire_datetime: Joi.date().required(),
+  is_process: Joi.boolean(),
+  voting_option: Joi.array().items(joiVoteOptionSchema).required(),
+  entire_count: Joi.number().required(),
+});
+
+const VoteSchema = new mongoose.Schema(joigoose.convert(joiVoteSchema));
+
+module.exports = mongoose.model("Vote", VoteSchema);
