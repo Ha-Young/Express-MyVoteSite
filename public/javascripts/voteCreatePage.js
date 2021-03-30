6e3b0b827e0b7839a2939797a1004ef6c2538c76
ha-young <hayeong28@naver.com>
2021-03-31 01:09:43 +0900

    add: voteCreate POST on ssr route

--- public/javascripts/voteCreatePage.js
+++ public/javascripts/voteCreatePage.js
@@ -3,6 +3,8 @@ const optionAddBtnElement = document.querySelector(".vote-option-add-btn");
 const optionListElement = document.querySelector(".vote-option-list");
 const voteCreateFormElement = document.querySelector(".vote-create-form");
 
+const LIMIT_HOUR = 1;
+
 function handleOptionRemoveBtnClick(e) {
   const optionItemElement = e.currentTarget.parentNode;
   const optionListElement = optionItemElement.parentNode;
@@ -24,6 +26,11 @@ function handleOptionAddBtnClick() {
 function handleVoteCreateSubmit(e) {
   e.preventDefault();
 
+  if (checkIsDateTimePast()) {
+    alert(`만료 날짜를 확인해주세요. 현재 시간보다 ${LIMIT_HOUR}시간 이후의 시간이어야 합니다.`);
+    return;
+  }
+
   const form = e.target;
 
   const dummyVoteOptionElement = document.createElement('textarea');
@@ -44,6 +51,21 @@ function getOptionObjectList() {
       title: optionInputElement.value,
     };
   });
+}
+
+function checkIsDateTimePast() {
+  const dateTimeElement = document.querySelector("input[type='datetime-local']");
+
+  console.log('checking time...', dateTimeElement.value);
+  const limitDate = new Date();
+  limitDate.setHours(limitDate.getHours() + LIMIT_HOUR);
+
+  console.log(limitDate);
+
+  const dateDiff = new Date(dateTimeElement.value) - limitDate;
+  console.log('dateDiff', dateDiff);
+
+  return dateDiff < 0;
 }
 
 function voteCreateInit() {
