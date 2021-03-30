7e7217ef135af427a2134ae70c91c800630fa066
JUNGJuhyoung <fheh1992@gmail.com>
2021-03-30 22:27:28 +0900

    Work on fetch voting on Atlas DB, Need to work css

--- routes/controller/votingsController.js
+++ routes/controller/votingsController.js
@@ -1,4 +1,15 @@
-exports.renderVotingsPage = function (req, res, next) {
-  console.log("requested user in votings controller", req.user);
-  res.status(200).render("votings");
+const Voting = require("../../models/Voting");
+const createError = require("http-errors");
+const errorMessage = require("../../constants/errorMessage");
+
+exports.renderVotingsPage = async function (req, res, next) {
+  try {
+    const votings = await Voting.find();
+    console.log(votings);
+
+    res.status(200).render("votings");
+  } catch (error) {
+    console.log(error);
+    next(createError(500, errorMessage.SERVER_ERROR));
+  }
 };
