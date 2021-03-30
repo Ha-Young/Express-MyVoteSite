ada0f829cfa83c0f769c06da483ae949cc9c2c69
ParkJeongbeen <pjb6510@gmail.com>
2021-03-30 16:44:46 +0900

    add logout endpoint

--- routes/login.js
+++ routes/login.js
@@ -7,5 +7,6 @@ const loginController = require("../controllers/login.controller");
 
 router.get("/", isNotAuthenticated, loginController.get);
 router.post("/", isNotAuthenticated, loginController.post);
+router.delete("/", loginController.delete);
 
 module.exports = router;
