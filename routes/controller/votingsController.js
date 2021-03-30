f7a3c294fa010585c812eafb0cc9771d11aac3d4
JUNGJUHYOUNG <fheh1992@gmail.com>
2021-03-31 02:17:29 +0900

    Add css in votings

--- routes/controller/votingsController.js
+++ routes/controller/votingsController.js
@@ -5,11 +5,23 @@ const errorMessage = require("../../constants/errorMessage");
 exports.renderVotingsPage = async function (req, res, next) {
   try {
     const votings = await Voting.find();
-    console.log(votings);
+    console.log("-----------------------------------------");
+    // console.log(votings);
 
-    res.status(200).render("votings");
+    const votingWithImg = insertRandomImage(votings);
+    console.log(votingWithImg);
+
+    res.status(200).render("votings", { votingWithImg });
   } catch (error) {
     console.log(error);
     next(createError(500, errorMessage.SERVER_ERROR));
   }
 };
+
+function insertRandomImage(array) {
+  for (let i = 0; i < array.length / 4; i++) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    array.splice(randomIndex, 0, "img");
+  }
+  return array;
+}
