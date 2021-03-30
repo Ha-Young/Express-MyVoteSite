12861f778545fb6b10c91ddb3cbc5cc2c94dff25
eluka22 <eluka22@gmail.com>
2021-03-30 16:17:04 +0900

    Add: remove options

--- public/javascripts/createVote.js
+++ public/javascripts/createVote.js
@@ -1,13 +1,39 @@
-const optionButton = document.querySelector("#optionButton");
-    const optionList = document.querySelector(".option-list");
-    let index = 0;
+const addOptionButton = document.querySelector("#optionButton");
+const optionList = document.querySelector(".option-list");
+const clearAllOptionButton = document.querySelector("#clearAllOptionsButton");
+let index = 0;
 
-    optionButton.addEventListener("click", () => {
-      const li = document.createElement("li");
-      const input = document.createElement("input");
-      input.setAttribute("name", "option_title");
-      input.setAttribute("data-index", index);
-      li.appendChild(input);
-      index++;
-      optionList.appendChild(li);
+addOptionButton.addEventListener("click", () => {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  const deleteButton = document.createElement("button");
+
+  input.setAttribute("name", "option_title");
+  input.setAttribute("type", "text");
+  input.setAttribute("data-index", index);
+
+  deleteButton.textContent = "DELETE";
+  deleteButton.setAttribute("type", "button");
+  deleteButton.classList.add("button", "deleteButton");
+
+  li.append(input, deleteButton);
+  optionList.appendChild(li);
+
+  index++;
+
+  removeOptions();
+});
+
+clearAllOptionButton.addEventListener("click", () => {
+  optionList.innerHTML = "";
+});
+
+function removeOptions() {
+  const deleteButtons = document.querySelectorAll(".deleteButton");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+      index--;
     });
+  }
+}
