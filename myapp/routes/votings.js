66ed4b1cf4c43d38805ba4bc6c1e0d1b969a0efe
kwon, minho <minhob38@gmail.com>
2021-03-31 01:05:20 +0900

    voting item page layout

--- myapp/routes/votings.js
+++ myapp/routes/votings.js
@@ -4,10 +4,7 @@ const Voting = require("../models/Voting");
 
 router.get("/:votingId", async (req, res, next) => {
   const { votingId } = req.params;
-  console.log(votingId);
   const voting = await Voting.findOne({ _id: votingId });
-
-  console.log(voting);
 
   res.status(200).render("votingDetail", {
     voting,
