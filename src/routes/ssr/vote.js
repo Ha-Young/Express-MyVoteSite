cf0f9a0bdeb5605337cb6b558690b239dbe8d867
ha-young <hayeong28@naver.com>
2021-03-31 01:09:43 +0900

    add: voteCreate POST on ssr route

--- src/routes/ssr/vote.js
+++ src/routes/ssr/vote.js
@@ -2,7 +2,7 @@ const createError = require("http-errors");
 const { celebrate, Joi } = require("celebrate");
 const { Router } = require("express");
 
-// const middlewares = require('../middlewares');
+const voteService = require("../../services/voteService");
 const { PREFIX, DETAIL, ERROR, NEW, SUCCESS } = require("../../config/routes").VOTE;
 const isLogin = require("../middlewares/isLogin");
 
@@ -14,4 +14,31 @@ module.exports = app => {
   route.get(NEW, (req, res) => {
     res.render("voteCreate");
   });
+
+  route.post(
+    NEW,
+    celebrate({
+      body: Joi.object({
+        title: Joi.string().required(),
+        datetime: Joi.string().required(),
+        vote_options: Joi.string().required(),
+      }),
+    }),
+    async (req, res, next) => {
+      console.log(req.body);
+
+      try {
+        const newVote = req.body;
+        newVote.vote_options = JSON.parse(vote.vote_options).map(option => ({
+          ...option,
+          count: 0,
+        }));
+
+        const { vote, error } = await voteService.createVote(newVote);
+
+      } catch (err) {
+        return next(createError(err));
+      }
+    }
+  );
 };
