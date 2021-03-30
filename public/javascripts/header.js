8304954186abc16a212d9d28419d66c716b9f25c
ParkJeongbeen <pjb6510@gmail.com>
2021-03-30 16:44:23 +0900

    add logout button in header

---
+++ public/javascripts/header.js
@@ -0,0 +1,22 @@
+const logoutButton = document.querySelector(".logout-button");
+
+const ACCESS_TOKEN = "accessToken";
+const REFRESH_TOKEN = "refreshToekn";
+
+const handleLogoutButtonClick = async (e) => {
+  try {
+    fetch("/login", {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "delete",
+    });
+  } catch (err) {
+    document.cookie = ACCESS_TOKEN + '=; Max-Age=0';
+    document.cookie = REFRESH_TOKEN + '=; Max-Age=0';
+  } finally {
+    window.location.href = "/";
+  }
+};
+
+logoutButton.addEventListener("click", handleLogoutButtonClick);
