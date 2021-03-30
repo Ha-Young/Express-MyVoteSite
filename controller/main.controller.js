83ccfc6c1de98b1b48c194e31a72e6b27fdbb084
Parksoyoon <maudlinsy@gmail.com>
2021-03-30 20:16:10 +0900

    Implement routers to votingList, votingView

--- controller/main.controller.js
+++ controller/main.controller.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const Voting = require('../models/Voting');
 
 exports.getMain = async (req, res, next) => {
-  console.log('main!!!');
   try {
     const allVotingList = await Voting.find({}).exec();
     console.log(allVotingList);
