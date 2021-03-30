cf2fecdf981d3430c216e5f149468e687b2b6b35
ha-young <hayeong28@naver.com>
2021-03-31 00:06:12 +0900

    add: voteCreate page front source - add option

---
+++ public/javascripts/templates/voteOptionItemTemplate.js
@@ -0,0 +1,8 @@
+function voteOptionItemTemplate() {
+  return `
+    <input type="text" value=""/>
+    <button class="vote-option-remove-btn" type="button">
+      <i class="fas fa-minus"></i>
+    </button>
+  `;
+}
