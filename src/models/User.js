08080c6ee7577b27e5e49d11366f384c7219c34e
ha-young <hayeong28@gmail.com>
2021-03-30 21:30:30 +0900

    add: Vote Scheme

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,8 @@ const { Joi } = require("celebrate");
 const mongoose = require("mongoose");
 const joigoose = require("joigoose")(mongoose);
 
+Joi.objectId = require('joi-objectid')(Joi);
+
 const joiUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string(),
@@ -9,6 +11,8 @@ const joiUserSchema = Joi.object({
   name: Joi.string().required(),
   provider: Joi.string(),
   isAdmin: Joi.boolean(),
+  my_votes: Joi.array().items(Joi.objectId),
+  voted_votes: Joi.array().items(Joi.objectId),
 });
 
 const UserSchema = new mongoose.Schema(joigoose.convert(joiUserSchema));
@@ -17,6 +21,8 @@ UserSchema.pre("save", function (next) {
   const user = this;
   try {
     user.isAdmin = false;
+    user.my_votes = [];
+    user.voted_votes = [];
 
     next();
   } catch (error) {
