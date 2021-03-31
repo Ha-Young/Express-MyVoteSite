d65764660fa54f107f3dfe96849e1eadbb103666
kwon, minho <minhob38@gmail.com>
2021-04-01 02:51:09 +0900

    Implement Voting Function

--- myapp/public/javascripts/votingDetail.js
+++ myapp/public/javascripts/votingDetail.js
@@ -2,20 +2,32 @@ const $detailItems = document.querySelectorAll(".detail-item");
 
 const onClickVotingOption = (ev) => {
   const xhr = new XMLHttpRequest();
-  const dbId = ev.target.dataset.id;
+  const votingId = ev.target.dataset.votingId;
+  const optionId = ev.target.dataset.optionId;
 
   xhr.onreadystatechange = () => {
+    console.log(xhr.readyState);
     if (xhr.readyState === 4 && xhr.status === 200) {
       console.log("AJAX Response Success");
-      console.log(xhr.responseText);
+      const { count } = JSON.parse(xhr.responseText);
+
+      const parseOptionContent = (content) => {
+        const splitIdx = content.lastIndexOf(" ");
+        const count = parseInt(content.slice(splitIdx), 10);
+        const text = content.slice(0, splitIdx);
+        return { count, text };
+      };
+
+      const { text } = parseOptionContent(ev.target.textContent);
+      ev.target.textContent = `${text} ${count}`;
     }
   };
 
   xhr.open("POST", "/ajax/changeVotingCount");
   xhr.setRequestHeader("Content-Type", "application/json");
 
-  const data = JSON.stringify({ dbId });
-  xhr.send(data);
+  const json = JSON.stringify({ votingId, optionId });
+  xhr.send(json);
 };
 
 $detailItems.forEach((dom) => {
--- myapp/routes/ajax.js
+++ myapp/routes/ajax.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const createError = require("http-errors");
+const mongoose = require("mongoose");
 const User = require("../models/User");
 const { findOneAndUpdate } = require("../models/Voting");
 const Voting = require("../models/Voting");
@@ -8,9 +9,19 @@ const router = express.Router();
 
 router.post("/changeVotingCount", async (req, res, next) => {
   // Voting.
-  const { _id } = req.body;
-  await findOneAndUpdate({ _id });
-  res.send("ajax");
+  const { votingId, optionId } = req.body;
+  console.log(votingId);
+  // // const votingOption = await Voting.findOne({ _id: dbId });
+  // votingOption = Voting.console.log(votingOption);
+  const voting = await Voting.findOne({ _id: votingId });
+  const option = voting.votingItems.id(optionId);
+  console.log(voting);
+  console.log(option);
+  option.count += 1;
+
+  await voting.save();
+
+  res.status(200).json({ count: option.count });
 });
 
 module.exports = router;
