fe62d3013f3ee81f09f6c6fa56d2c8b4d0b155f9
Hansol Yun <hsyoon315@gmail.com>
2021-04-01 01:41:05 +0900

    Add date conversion util function

---
+++ utils/conversion.js
@@ -0,0 +1,19 @@
+const dayjs = require("dayjs");
+
+exports.convert = function (createdAt, day, hour, minute) {
+  let expiredAt = createdAt;
+
+  if (!isNaN(day)) {
+    expiredAt = dayjs(expiredAt).add(Number(day), "day");
+  }
+
+  if (!isNaN(hour)) {
+    expiredAt = dayjs(expiredAt).add(Number(hour), "hour");
+  }
+
+  if (!isNaN(minute)) {
+    expiredAt = dayjs(expiredAt).add(Number(minute), "minute");
+  }
+
+  return dayjs(expiredAt).format("YYYY-MM-DD HH:mm:ss");
+}
