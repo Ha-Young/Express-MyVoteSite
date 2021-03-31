d92fe9f3350944ee5c994a341d0d1161d4fdd3f5
minwoo kim <vvee1253@gmail.com>
2021-04-01 05:21:36 +0900

    Add dashboard design

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const voteController = require('../controllers/vote.controller');
 const { checkAuthenticated } = require('../middlewares/auth');
+const Vote = require('../models/Vote');
 
 /* GET home page. */
 router.get('/', require('./home'));
-router.get('/dashboard', (req, res, next) => {
+router.get('/dashboard', async (req, res, next) => {
+  const votes = await Vote.find({}).lean();
+  if (!req.user) {
+    req.user = {
+      name: "kim",
+      avatarUrl: "https://storage.jewheart.com/content/users/avatars/1606/avatar_1606_500.jpg?1558623487"
+    }
+  }
+  res.locals.user = req.user;
+  res.locals.votes = votes;
   res.render('dashboard');
 })
-router.get('/myvote', checkAuthenticated,voteController.renderMyVote);
+router.get('/myvotes', checkAuthenticated, voteController.renderMyVote);
+
 router.use('/home', require('./home'));
 router.use('/auth', require('./auth'));
 router.use('/vote', require('./vote'));
