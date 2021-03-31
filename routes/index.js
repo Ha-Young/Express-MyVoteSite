e5cccefbb7975cab36a0984f4a3dbe1699c99300
ParkJeongbeen <pjb6510@gmail.com>
2021-03-31 17:15:32 +0900

    add VotingList in index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.render("index", {
-    user: { email: res.locals.userEmail || "Guest" }
-  });
+const Voting = require("../models/Voting");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const votingList = await Voting.find().lean();
+    res.render("index", {
+      user: { email: res.locals.userEmail || "Guest" },
+      votingList,
+    });
+  } catch (err) {
+    next(createError(500, err.message));
+  }
 });
 
 module.exports = router;
