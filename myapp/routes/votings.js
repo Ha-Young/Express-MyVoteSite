516bfec4151c51881e012d23b5020e47660adb4f
kwon, minho <minhob38@gmail.com>
2021-03-31 15:02:31 +0900

    Add Date in Layout

--- myapp/routes/votings.js
+++ myapp/routes/votings.js
@@ -5,9 +5,55 @@ const Voting = require("../models/Voting");
 router.get("/:votingId", async (req, res, next) => {
   const { votingId } = req.params;
   const voting = await Voting.findOne({ _id: votingId });
+  const {
+    author,
+    title,
+    description,
+    votingItem,
+    voters,
+    startTime,
+    endTime,
+  } = voting;
+
+  const getLocalTime = (ISOTime) => {
+    const year = ISOTime.getFullYear();
+    const month = ISOTime.getMonth() + 1;
+    const date = ISOTime.getDate();
+    const hour = ISOTime.getHours();
+    const minute = ISOTime.getMinutes();
+
+    const localTime = {
+      year,
+      month,
+      date,
+      hour,
+      minute,
+    };
+
+    return localTime;
+  };
+
+  const getProgress = (ISOTime) => {
+    if (ISOTime.getTime() > new Date().getTime()) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const startLocalTime = getLocalTime(startTime);
+  const endLocalTime = getLocalTime(endTime);
+  const isClosed = getProgress(endTime);
 
   res.status(200).render("votingDetail", {
-    voting,
+    author,
+    title,
+    description,
+    votingItem,
+    voters,
+    startLocalTime,
+    endLocalTime,
+    isClosed,
   });
 });
 
