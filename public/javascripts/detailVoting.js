7fa65b3aa72450f47f4643ac372300e02fa8e1c6
JUNGJuhyoung <fheh1992@gmail.com>
2021-03-31 22:58:14 +0900

    Work on chart

--- public/javascripts/detailVoting.js
+++ public/javascripts/detailVoting.js
@@ -1,13 +1,23 @@
-console.log("hi");
+const votingOptions = document.querySelectorAll(".detail-voting-option");
+console.log("votingOption", votingOptions);
+
+const chart = {
+  labels: [],
+  data: [],
+};
+
+for (const option of votingOptions) {
+  console.log("option", option);
+}
 
 const ctx = document.getElementById("myChart");
 const myChart = new Chart(ctx, {
-  type: "pie",
+  type: "doughnut",
   data: {
     labels: ["사과", "사과"],
     datasets: [
       {
-        data: [12, 19],
+        data: [25, 1],
         backgroundColor: getRandomColor(),
         borderWidth: 1,
       },
