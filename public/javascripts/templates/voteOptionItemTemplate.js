f8d22c0e5a5ad1a026c5f152cffcb4b92205f9d4
ha-young <hayeong28@gmail.com>
2021-03-31 21:42:52 +0900

    add: infiniteScroll! on index page

--- public/javascripts/templates/voteOptionItemTemplate.js
+++ public/javascripts/templates/voteOptionItemTemplate.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-undef */
-window.templates = {
-  ...window.templates,
-  voteOptionItemTemplate: function () {
+const voteOptionItemTemplate = (function () {
+  return (function () {
     return `
       <input type="text" value=""/>
       <button class="vote-option-remove-btn" type="button">
         <i class="fas fa-minus"></i>
       </button>
     `;
-  },
+  });
+});
+
+window.templates = {
+  ...window.templates,
+  voteOptionItemTemplate,
 };
