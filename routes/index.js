9c49e0d8278fe567dbe37ee2ded0eeb2b600ffa2
minwoo kim <vvee1253@gmail.com>
2021-04-01 00:33:00 +0900

    Add dashboard

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const { checkAuthenticated } = require('../middlewares/auth');
 
 /* GET home page. */
 router.get('/', require('./home'));
+router.get('/dashboard', (req, res, next) => {
+  res.render('dashboard');
+})
 router.get('/myvote', checkAuthenticated,voteController.renderMyVote);
 router.use('/home', require('./home'));
 router.use('/auth', require('./auth'));
