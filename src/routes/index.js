868349627474b103b3dcac77db9614e8077812c5
ha-young <hayeong28@naver.com>
2021-04-02 03:25:29 +0900

    fix: isLogin to authChecker for only verify jwt token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const authChecker = require("./middlewares/authChecker");
 const { API } = require("../config/routes");
 const apiRoute = require("./api");
 const ssrRoute = require("./ssr");
 
 module.exports = app => {
+  app.use(authChecker);
   app.use("/", ssrRoute());
   app.use(API.PREFIX, apiRoute());
 };
