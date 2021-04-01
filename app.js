db0419794fd3d678df4c27ab0eae1d98c642f6ab
Hansol Yun <hsyoon315@gmail.com>
2021-04-02 06:07:32 +0900

    Add flash and remove comment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
+const flash = require("connect-flash");
 
 const configs = require("./configs");
 require("./configs/passport");
@@ -25,13 +26,11 @@ db.on("error", () => console.log("error"));
 db.once("open", () => console.log("connect!!!"));
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const votingsRouter = require("./routes/votings");
 
 const app = express();
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -47,26 +46,23 @@ app.use(session({
   saveUninitialized: false,
 }));
 
+app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
-app.use("/users", usersRouter);
 app.use("/votings", votingsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
