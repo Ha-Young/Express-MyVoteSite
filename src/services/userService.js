f5cfccfee6fabc891ee291a2ac27144c60da39b7
ha-young <hayeong28@gmail.com>
2021-04-02 03:34:45 +0900

    add: voteToOption API - add user votedlist

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,12 +6,21 @@ exports.VoteToOption = async ({ userId, voteId }) => {
 
     const userRecord = await User.findById(userId);
 
+    if (!userRecord) {
+      throw new Error("can not find user");
+    }
+
     console.log('userRecord', userRecord);
 
     userRecord.voted_votes.push(voteId);
 
     console.log('vote userRecord', userRecord);
 
+    userRecord.save();
+
+    const user = userRecord.toObject();
+
+    return { user };
   } catch (error) {
     return { error };
   }
