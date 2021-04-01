baf9b444eb5ce1d83e2e5e51c771416925ab1f90
eluka22 <eluka22@gmail.com>
2021-04-01 19:57:58 +0900

    Fix: validation

--- routes/middlewares/validator.js
+++ routes/middlewares/validator.js
@@ -3,42 +3,40 @@ const format = require("date-fns/format");
 const { getUserInfo } = require("../../util/jwtHelper");
 const bcrypt = require("bcrypt");
 const User = require("../../models/User");
-const Vote = require("../../models/Vote");
 
 exports.validateUser = [
   check("email")
     .isEmail()
-    .withMessage("Invalid email address")
+    .withMessage("유효하지 않은 이메일입니다.")
     .custom(async value => {
       const user = await User.findOne({ email: value });
       if (user) {
-        throw new Error("User with this email is already existed");
+        throw new Error("이미 존재하는 이메일입니다.");
       }
       return true;
     }),
   check("name")
-    .isLength({ min: 3, max: 10 })
-    .withMessage("Name should have minimum length of 2"),
+    .isLength({ min: 2, max: 10 })
+    .withMessage("이름은 최소 2글자 이상이어야 합니다."),
   check("password")
     .isLength({ min: 8, max: 12 })
-    .withMessage("Password should have min and max length between 8 - 12")
+    .withMessage("비밀번호는 8자리에서 12자리 사이여야 합니다.")
     .matches(/\d/)
-    .withMessage("Password should have at least one number")
+    .withMessage("비밀번호는 최소 하나의 숫자를 포함해야 합니다.")
     .matches(/[!@#$%^&*(),.?":{}|<>]/)
-    .withMessage("Password should have at least one special character"),
+    .withMessage("비밀번호는 최소 하나의 특수문자를 포함해야 합니다."),
   check("confirmedPassword")
     .custom((value, { req }) => {
       if (value !== req.body.password) {
-        throw new Error("Confirmed password does not match");
+        throw new Error("비밀번호를 동일하게 입력해주세요.");
       }
       return true;
     }),
   (req, res, next) => {
     const result = validationResult(req);
-    const user = getUserInfo(req.cookies);
 
     if (!result.isEmpty()) {
-      return res.status(422).render("signup", { error: result.errors[0], user });
+      return res.status(422).json({ error: result.errors[0] });
     }
     next();
   }
@@ -47,11 +45,11 @@ exports.validateUser = [
 exports.validateLogin = [
   check("email")
     .isEmail()
-    .withMessage("Invalid email address")
+    .withMessage("유효하지 않은 이메일입니다.")
     .custom(async value => {
       const user = await User.findOne({ email: value });
       if (!user) {
-        throw new Error("Email is not existed.");
+        throw new Error("존재하지 않는 이메일입니다.");
       }
       return true;
     }),
@@ -60,7 +58,7 @@ exports.validateLogin = [
       const user = await User.findOne({ email: req.body.email });
       const isMatched = await bcrypt.compare(value, user.password);
       if (!isMatched) {
-        throw new Error("Password is incorrect.");
+        throw new Error("비밀번호를 확인해주세요.");
       }
       return true;
     }),
@@ -77,35 +75,31 @@ exports.validateLogin = [
 exports.validateCreatingVote = [
   check("title")
     .isLength({ min: 1})
-    .withMessage("Title must be required."),
+    .withMessage("투표 제목을 입력해주세요."),
   check("expiration_date")
     .custom(value => {
       const today = format(new Date(), "yyyy-MM-dd'T'HH:mm");
       if (value < today) {
-        throw new Error("Date and time must be later than today");
+        throw new Error("만료기간은 현 시각보다 늦어야합니다.");
       }
       return true;
     }),
   check("option_title")
     .isLength({ min: 1 })
-    .withMessage("Option title must be required")
+    .withMessage("옵션 제목을 입력해야 합니다.")
     .custom(value => {
-      console.log(value);
       if (!Array.isArray(value) || value.length < 2) {
-        throw new Error("Add more options (at least 2)");
+        throw new Error("옵션은 최소 두 개 이상이어야 합니다.");
       }
       if (value.length > 8) {
-        throw new Error("Max options can be 8");
+        throw new Error("옵션은 최대 8개까지 가능합니다.");
       }
       return true;
     }),
   (req, res, next) => {
     const result = validationResult(req);
-    const user = getUserInfo(req.cookies);
-    console.log(req.body);
 
     if (!result.isEmpty()) {
-      // return res.status(422).render("votings-new", { error: result.errors[0], user });
       return res.status(422).json({ error: result.errors[0] });
     }
 
@@ -122,11 +116,11 @@ exports.validateCastingVote = [
       const isCasted = userInfo.casted_votes.find(vote => vote.toString() === id);
 
       if (value.length === 0) {
-        throw new Error("You should choice at least one option");
+        throw new Error("최소 하나의 옵션을 선택해야 합니다.");
       }
 
       if (isCasted) {
-        throw new Error("Already voted");
+        throw new Error("이미 투표를 했습니다.");
       }
 
       return false;
