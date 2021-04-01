6130b8535779b320a3b84c53834a67302a763dd7
eluka22 <eluka22@gmail.com>
2021-04-01 19:58:31 +0900

    Remove: unnecessary function

--- util/jwtHelper.js
+++ util/jwtHelper.js
@@ -1,20 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-exports.generateAccessToken = (user, key, expiredDate) => {
+exports.generateToken = (user, key, expiredDate) => {
   return jwt.sign({ name: user.name, email: user.email }, key, { expiresIn: expiredDate });
 };
 
-exports.refreshAccessToken = (refreshToken) => {
-  if (refreshToken === null) return res.status(401);
-
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403);
-
-    return this.generateAccessToken(user, process.env.ACCESS_TOKEN_SECRET, "15m");
-  });
-};
-
 exports.getUserInfo = (cookies) => {
+  console.log(cookies);
   if (cookies.accessToken) {
     return jwt.verify(cookies.accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return null;
