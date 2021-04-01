d6d39a386a1d32c3dfe98a078b9df8e7afd3854e
eluka22 <eluka22@gmail.com>
2021-04-01 17:03:51 +0900

    Fix: vote result

--- public/javascripts/voteResult.js
+++ public/javascripts/voteResult.js
@@ -6,29 +6,35 @@ const optionsCount = document.querySelectorAll(".vote-result li .count");
 const optionsLabel = Array.prototype.slice.call(optionsTitle).map(title => title.textContent);
 const optionsData = Array.prototype.slice.call(optionsCount).map(count => Number(count.textContent));
 
-const context = document.querySelector("#vote-chart").getContext("2d");
-const chart = new Chart(context, {
-  type: "pie",
-  data: {
-    labels: optionsLabel,
-    datasets: [{
-      data: optionsData,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)'
-      ],
-      borderWidth: 0
-    }]
-  },
-  options: {
-    legend: {
-      position: "left"
+const canvas = document.querySelector("#vote-chart");
+
+if (canvas) {
+  const chart = new Chart(canvas.getContext("2d"), {
+    type: "pie",
+    data: {
+      labels: optionsLabel,
+      datasets: [{
+        data: optionsData,
+        backgroundColor: [
+          "rgba(90, 60, 255, 1)",
+          "rgba(253, 255, 82, 1)",
+          "rgba(232, 44, 94, 1)",
+          "rgba(255, 170, 48, 1)",
+          "rgba(100, 217, 209, 0.85)",
+          "rgba(192, 48, 255, 1)",
+          "rgba(255, 100, 90, 1)",
+          "rgba(48, 255, 183, 1)"
+        ],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      legend: {
+        position: "left"
+      }
     }
-  }
-});
+  });
+}
 
 backButton.addEventListener("click", () => {
   const previousUrl = document.referrer;
