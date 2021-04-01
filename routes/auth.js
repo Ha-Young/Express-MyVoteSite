f3edd581bffea6fe1cdc1290092cacd511c8705a
Hansol Yun <hsyoon315@gmail.com>
2021-04-02 06:04:35 +0900

    Add redirect to previous page

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const createError = require("http-errors");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
@@ -8,19 +7,41 @@ const authenticateUser = require("./middlewares/authenticateUser");
 const passport = require("passport");
 
 router.get("/login", (req, res, next) => {
-  res.status(200).render("login");
+  try {
+    const flashMessage = req.flash();
+    if (flashMessage.error) {
+      res.status(200).render("login", { errorMessage: flashMessage.error[0] });
+    } else {
+      res.status(200).render("login");
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/login",
   passport.authenticate("local", {
-    failureRedirect: "/auth/signup",
-    successRedirect: "/"
+    failureRedirect: "/auth/login",
+    failureFlash: true,
   }
-));
+), (req, res, next) => {
+  const redirectUrl = req.headers.referer.replace("http://localhost:3000/auth/login?url=", "");
+  const loginUrl = "http://localhost:3000/auth/login";
+
+  if (redirectUrl !== "undefined" && redirectUrl !== loginUrl) {
+    res.redirect(redirectUrl);
+  } else {
+    res.redirect("/");
+  }
+});
 
 router.get("/logout", authenticateUser, (req, res, next) => {
-  req.logout();
-  res.status(301).redirect("/");
+  try {
+    req.logout();
+    res.status(301).redirect("/");
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/signup", (req, res, next) => {
@@ -44,7 +65,7 @@ router.post("/signup", async (req, res, next) => {
       password,
     });
 
-    res.status(200).render("success", { message: "회원가입을 축하합니다" });
+    res.status(200).render("success", { message: "회원가입을 축하합니다", state: "signup" });
   } catch (err) {
     next(err);
   }
