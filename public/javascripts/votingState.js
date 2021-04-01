54da0678e92b739a681c5c1ccf0445bfdc682eb6
Hansol Yun <hsyoon315@gmail.com>
2021-04-01 21:29:33 +0900

    Add hidden classname

--- public/javascripts/votingState.js
+++ public/javascripts/votingState.js
@@ -1,6 +1,8 @@
 (function () {
   const currentDate = new Date();
   const votes = document.querySelector(".votes");
+  const showChart = document.querySelector(".show-chart");
+  const formContainer = document.querySelector(".form-container");
 
   for (let i = 0; i < votes.children.length; i++) {
     const expiredAt = votes.children[i].children[2].textContent;
@@ -8,8 +10,10 @@
 
     if (new Date(expiredAt) < currentDate) {
       voteState.textContent = "만료";
+      if (formContainer) formContainer.className = "hidden";
     } else {
       voteState.textContent = "진행중";
+      if (showChart) showChart.className = "hidden";
     }
   }
-})()
+})();
