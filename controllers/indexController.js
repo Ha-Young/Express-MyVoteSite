2d235b3a000b24d68d7743414c45719689afa943
onemo <wonmotime1@naver.com>
2021-04-01 21:25:03 +0900

    add css in signup

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -21,34 +21,3 @@ exports.getMainPage = async (req, res) => {
 
   res.render("index", { progressingVote, endedVote, isUserLogedIn });
 };
-
-{/* <div class="vote-wrapper">
-<% for (let i = 0; i < votes.length; i++) { %>
-  <a href="/votings/<%= votes[i]._id %>">
-    <div class="vote">
-      <div class="title">
-        제목: <%= votes[i].title %>
-      </div>
-      <div class="creator">
-        작성자: <%= votes[i].creator %>
-      </div>
-      <div class="expire-date">
-        만료일:
-        <%=
-          votes[i].endDate.getFullYear() + "년 " +
-          (votes[i].endDate.getMonth() + 1) + "월" +
-          votes[i].endDate.getDate() + "일"
-        %>
-      </div>
-      <div class="vote-status">
-        <% if (votes[i].isOnVote === "true") { %>
-          <div>⭕️ 투표 진행중 ⭕️</div>
-        <% } else { %>
-          <div>❌ 투표 마감 ❌</div>
-        <% } %>
-      </div>
-    </div>
-  </a>
-<% } %>
-</div>
-</div> */}
