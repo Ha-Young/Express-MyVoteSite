44a02b3aff06f3ac626fa7b8535bc4656d174eab
Hansol Yun <hsyoon315@gmail.com>
2021-04-01 21:31:40 +0900

    Fix put vote router

--- routes/votings.js
+++ routes/votings.js
@@ -7,7 +7,6 @@ const authenticateUser = require("../routes/middlewares/authenticateUser");
 const Vote = require("../models/Vote");
 const { convert } = require("../utils/conversion");
 
-
 router.get("/new", authenticateUser, (req, res, next) => {
   res.status(200).render("newVoting");
 });
@@ -16,7 +15,6 @@ router.post("/new", authenticateUser, async (req, res, next) => {
   const { title, day, hour, minute, option } = req.body;
   const author = req.user.name;
   const createdAt = dayjs().format("YYYY-MM-DD HH:mm:ss");
-
   const expiredAt = convert(createdAt, day, hour, minute);
 
   if (expiredAt === createdAt) {
@@ -34,7 +32,7 @@ router.post("/new", authenticateUser, async (req, res, next) => {
     author,
     createdAt,
     expiredAt,
-    options
+    options,
   });
 });
 
@@ -46,47 +44,47 @@ router.get("/:id", async (req, res, next) => {
   try {
     const voteId = req.params.id;
     const vote = await Vote.findOne({ _id: voteId });
-    const name = req.user.name;
+    let name;
+
+    if (req.user) {
+      name = req.user.name;
+    }
+
     res.status(200).render("voting", { vote, name });
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/:id", async (req, res, next) => {
-  if (!req.user) {
-    res.status(200).render("failure", { message: "로그인 후 이용해주세요", state: "login" });
-    return;
-  }
-
-  const voteId = req.params.id;
-  const choice = req.body.option;
-  const userId = req.user._id;
-
+router.put("/:id", authenticateUser, async (req, res, next) => {
   try {
+    const voteId = req.params.id;
+    const optionId = req.body.id;
+    const userId = req.user._id;
+
     const vote = await Vote.findOne({_id: voteId});
     const isParticipated = vote.participated_users.some(user =>
       user.toString() === userId.toString()
     );
 
     if (isParticipated) {
-      res.render("failure", { message: "이미 참여하신 투표군요.." });
+      res.status(200).json({"participated": true});
       return;
     }
 
+    vote.options.map((option) => {
+      if (option._id.toString() === optionId) {
+        option.count += 1;
+      }
+    });
+
+    await vote.save();
     await Vote.findOneAndUpdate(
       {_id: voteId},
       {$addToSet: {participated_users: [ userId ]}}
     );
 
-    for (let i = 0; i < vote.options.length; i++) {
-      if (vote.options[i].content === choice) {
-        vote.options[i].count += 1;
-      }
-    }
-
-    vote.save();
-    res.render("success", { message: "투표에 참여해주셔서 감사합니다", state: "vote" });
+    res.status(200).json({"success": true});
   } catch (err) {
     next(err);
   }
