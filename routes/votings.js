a7ce1c63c94801b1d0cd27db62017cef03ff96cc
Hansol Yun <hsyoon315@gmail.com>
2021-04-03 06:17:46 +0900

    Refactor

--- routes/votings.js
+++ routes/votings.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const authenticateUser = require("../routes/middlewares/authenticateUser");
+const authenticateUserToSubmit = require("../routes/middlewares/authenticateUserToSubmit");
 const votingsController = require("../routes/controllers/votings.controller");
 
 router.get("/success", votingsController.renderSuccess);
 
-router.get("/new", authenticateUser, votingsController.renderNewVoting);
-router.post("/new", authenticateUser, votingsController.postNewVoting);
+router.get("/new", authenticateUser , votingsController.renderNewVoting);
+router.post("/new", authenticateUser , votingsController.postNewVoting);
 
 router.get("/:id", votingsController.renderVoting);
-router.put("/:id", authenticateUser, votingsController.castVote);
-router.delete("/:id", authenticateUser, votingsController.deleteVote);
+router.put("/:id", authenticateUserToSubmit, votingsController.castVote);
+router.delete("/:id", authenticateUserToSubmit, votingsController.deleteVote);
 
 module.exports = router;
