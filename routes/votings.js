de282665b7817a47961cd67395a7fb1284688421
Hansol Yun <hsyoon315@gmail.com>
2021-04-02 18:27:09 +0900

    Remove error router and inefficient path

--- routes/votings.js
+++ routes/votings.js
@@ -4,14 +4,7 @@ const router = express.Router();
 const authenticateUser = require("../routes/middlewares/authenticateUser");
 const votingsController = require("../routes/controllers/votings.controller");
 
-const Vote = require("../models/Vote");
-const User = require("../models/User");
-
 router.get("/success", votingsController.renderSuccess);
-
-// router.get("/error", (req, res, next) => {
-//   res.status(400).render("error");
-// });
 
 router.get("/new", authenticateUser, votingsController.renderNewVoting);
 router.post("/new", authenticateUser, votingsController.postNewVoting);
