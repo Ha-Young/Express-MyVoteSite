ed1b95448b00a3e4db4851c7727efaf6c827c327
Hansol Yun <hsyoon315@gmail.com>
2021-04-02 18:24:13 +0900

    Create constants

---
+++ constants/resultMessage.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  INVALID_EMAIL: "이미 존재하는 이메일 입니다",
+  INVALID_PERIOD: "만료 기간 및 시간을 입력하세요",
+  SUCCESS_SIGNUP: "회원가입을 축하합니다",
+  SUCCESS_CREATE_VOTE: "새로운 투표가 생성되었습니다",
+});
