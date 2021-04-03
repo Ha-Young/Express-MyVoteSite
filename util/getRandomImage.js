0d327ee12c808d338938a937e4bec292b054ceab
kimmin920 <vvee1253@gmail.com>
2021-04-03 15:00:33 +0900

    Change name of random images

--- util/getRandomImage.js
+++ util/getRandomImage.js
@@ -1,4 +1,4 @@
-const images = [
+const voteImages = [
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT406BUwUOLUgksjp2ZU5JA_bfLjiaZVADnng&usqp=CAU',
   'https://www.cnet.com/a/img/arY9B-RgjY0blw5S-AuAmIk5-LA=/1200x630/2020/08/24/d099aab7-e826-45e5-af94-896bb04b2d10/vote-elections-2020-trump-pence-biden-harris-0656.jpg',
   'https://saportakinsta.s3.amazonaws.com/wp-content/uploads/2020/08/Vote-Your-Voice_main-Hero_1800x1012.png',
@@ -9,4 +9,4 @@ const images = [
   'https://media.npr.org/assets/img/2018/09/06/gettyimages-1007970946_wide-b63fe9178fc3ed494c31073ecd9e3e1a831633f0-s800-c85.jpg',
 ];
 
-exports.getRandomImage = () => images[Math.floor(Math.random() * images.length)];
+exports.getRandomImage = () => voteImages[Math.floor(Math.random() * voteImages.length)];
