12c81a3d997d6c3b7028d666de29950f30ad9251
kimmin920 <vvee1253@gmail.com>
2021-04-03 14:58:35 +0900

    Add constant for currenttime

--- util/getCurrentTime.js
+++ util/getCurrentTime.js
@@ -1,5 +1,8 @@
+const NINE_HOURS = 9 * 3600000;
+
 function getFormattedCurrentDateTime() {
-  return new Date(Date.now() + 9 * 3600000).toISOString().substr(0, 16);
+  // add 9 hours, to get KST(Korea Standard Time, GMT+0900)
+  return new Date(Date.now() + NINE_HOURS).toISOString().substr(0, 16);
 }
 
 exports.getFormattedCurrentDateTime = getFormattedCurrentDateTime;
