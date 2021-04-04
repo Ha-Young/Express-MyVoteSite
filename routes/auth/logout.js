cc4376caccfc287d876bbb98ad1e83760de9f3b2
hyerichung <hyerichungonearth@gmail.com>
2021-04-04 15:12:02 +0900

    Fix path

--- routes/auth/logout.js
+++ routes/auth/logout.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/logoutController");
+const authController = require("../../controllers/authController");
 
 router.get("/", authController.getLogOut);
 
