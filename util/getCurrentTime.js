0d0a7b06f0681cd439d52bd7829873534f335285
minwoo kim <vvee1253@gmail.com>
2021-04-04 12:51:43 +0900

    Delete comment

--- util/getCurrentTime.js
+++ util/getCurrentTime.js
@@ -1,7 +1,6 @@
 const NINE_HOURS = 9 * 3600000;
 
 function getFormattedCurrentDateTime() {
-  // add 9 hours, to get KST(Korea Standard Time, GMT+0900)
   return new Date(Date.now() + NINE_HOURS).toISOString().substr(0, 16);
 }
 
